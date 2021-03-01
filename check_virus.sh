7e9f2e5cec6d5f58080560dbe8ca0f119d38b9e2
Petyr-CZ <novakpetr531@gmail.com>
2021-03-01 21:38:38 +0100

    Add files via upload
    added comments

--- check_virus.sh
+++ check_virus.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+
+# page with Covid-19 informations (CZECH)
 wget -q 'https://onemocneni-aktualne.mzcr.cz/covid-19'
 
-
+# variables to get sensible sentence
 pripady=$(grep -A 2 'Aktivní případy' covid-19 | tail -n1 | cut -d'"' -f2)
 datum=$(grep -A 7 'Aktivní případy' covid-19 | tail -n1 | cut -c51- | tr -d '&nbsp;' | cut -d'v' -f1)
 cas=$(grep -A 7 'Aktivní případy' covid-19 | tail -n1 | cut -d';' -f5 | tr -d '&nbsp' | tr '.' ':')
@@ -11,7 +13,7 @@ chmod 644 /home/pi/Bot/vir.txt
 stary="/home/pi/Bot/vir.txt"
 novy="/home/pi/Bot/vir_check.txt"
 
-
+# check if number of cases changed, tweet new number if did
 if cmp -s "$stary" "$novy"; then
 	echo 'Nic nového\n'
 
@@ -21,5 +23,5 @@ else
 	sh /home/pi/Bot/run_virus.sh
 fi
 
-
+# removes residual files
 rm covid*
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #sh /home/pi/Bot/pripady.sh
-echo "$(/opt/vc/bin/vcgencmd measure_temp)" | cut -c6- > /home/pi/Bot/pi_temp.txt
-echo "Precise time in London is $(date '+%H : %M : %S, %N').\nMy brain's temperature is $(/opt/vc/bin/vcgencmd measure_temp | cut -c6-11)." > /home/pi/Bot/data.txt | python /home/pi/Bot/bot_tweet.py
+#echo "$(/opt/vc/bin/vcgencmd measure_temp)" | cut -c6- > /home/pi/Bot/pi_temp.txt
+echo "Precise time in London is $(date '+%H : %M : %S, %N').\n" > data.txt | python bot_tweet.py
