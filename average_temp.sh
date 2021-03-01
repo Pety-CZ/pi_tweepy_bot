c4ced493572c4ba389ab5d46ef1bb4e6d84e5e6b
Petyr-CZ <novakpetr531@gmail.com>
2021-03-01 21:33:55 +0100

    Delete average_temp.sh

--- average_temp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#grep 'temperature' /var/mail/pi | tail -n18 | cut -d' ' -f5 | cut -c1-4 | awk 'BEGIN{pocet=0;soucet=0} {pocet+=1;soucet+=$1} END{printf "%dC\n", soucet/pocet}'
-grep 'temperature' /var/mail/pi | cut -d' ' -f5 | tr -d '\n' | tr 'C' '\n' | tr -d ".\'" |  awk 'BEGIN{pocet=0;soucet=0} {pocet+=1;soucet+=$1} END{printf "%f°C\n", soucet/pocet/10}' >> temp.txt
